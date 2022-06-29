42759375198f335264b6b5f0cff4d3aa3895fbf1
Ronald <ronald.isael@hotmail>
2022-06-29 17:54:28 -0400

    Se Agrego la ruta PUT (PARA ACTUALIZAR LOS DATOS)

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,36 +1,59 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const routermovies = Router();
 const movies = require('../sample.json');
 const _underscore = require('underscore');
 
 //Ruta para que me de un resultado
 routermovies.get('/', (req, res) => {
-    
+
     res.json(movies);
 });
 //Ruta para recibir datos
- routermovies.post('/', (req, res) => {
-    const {title, director, year, reaiting} = req.body;
+routermovies.post('/', (req, res) => {
+    const { title, director, year, reaiting } = req.body;
 
-    if(title && director && year && reaiting){
+    if (title && director && year && reaiting) {
         const id = movies.length + 1;
-        const newmovies = {...req.body, id}
+        const newmovies = { ...req.body, id }
         movies.push(newmovies);
         res.json(movies);
-    }else {
-        res.status(500).json({"error": "Theare was an error."});
+    } else {
+        res.status(500).json({ "error": "Theare was an error." });
     }
- })
+});
+
+//Ruta para actualizar los datos
+routermovies.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, reaiting } = req.body;
+
+    if (title && director && year && reaiting) {
+        _underscore.each(movies, (m, i) => {
+            if (m.id == id) {
+                m.title = title;
+                m.director = director;
+                m.year = year;
+                m.raiting =reaiting;
+            }
+        });
+
+    } else {
+        res.status(500).json({ "error": "Theare was an error." });
+    }
+
+    res.send(movies);
+});
+
 //Ruta para eliminar algun dato
- routermovies.delete('/:id', (req, res) => {
-    const {id} = req.params;
+routermovies.delete('/:id', (req, res) => {
+    const { id } = req.params;
     console.log(id);
     _underscore.each(movies, (m, i) => {
-        if(m.id == id){
+        if (m.id == id) {
             movies.splice(i, 1);
         }
     });
     res.send(movies);
- });
+});
 
 module.exports = routermovies;
